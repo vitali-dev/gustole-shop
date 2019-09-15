5bce6867f008bf0d62eeae42051ba2dab1883997
vitali-dev <vitali.repin@gmail.com>
2019-09-15 17:44:01 +0200

    added cartIcon and cartDropdown components to our application, also implmented cart reducer

---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,5 @@
+import CartActionTypes from './cart.typyes';
+
+export const toggleCartHidden = () => ({
+    type: CartActionTypes.TOGGLE_CART_HIDDEN
+});
---
+++ src/redux/cart/cart.typyes.js
@@ -0,0 +1,5 @@
+const CartActionTypes = {
+    TOGGLE_CART_HIDDEN: 'TOGGLECART_HIDDEN'
+};
+
+export default CartActionTypes;
