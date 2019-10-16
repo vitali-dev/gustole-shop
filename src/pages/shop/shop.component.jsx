6081a1d8b336ba4a16e0f7a3d8d943ae1d84325a
vitali-dev <vitali.repin@gmail.com>
2019-10-16 18:09:50 +0200

    added directory state into redux and collection state into redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+const ShopPage = ({collections }) => (
+    <div className='shop-page'>
+        <CollectionsOverview />
+    </div>
+);
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-              collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionsProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionsProps} />
-                    ))
-                }
-            </div>
-        )
-    }
-}
 
 export default ShopPage;
